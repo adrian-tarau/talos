f8a6fba799c41476b5ddfeab501bb7b9e95b6572
Adrian Tarau <adrian.tarau@gmail.com>
2025-01-29 08:58:30 -0500

    Switch to light header, display some performance charts

--- report/src/main/java/net/microfalx/maven/report/ChartHelper.java
+++ report/src/main/java/net/microfalx/maven/report/ChartHelper.java
@@ -1,18 +1,22 @@
 package net.microfalx.maven.report;
 
+import net.microfalx.jvm.ServerMetrics;
+import net.microfalx.jvm.VirtualMachineMetrics;
+import net.microfalx.lang.Nameable;
 import net.microfalx.lang.NamedIdentityAware;
-import net.microfalx.lang.StringUtils;
 import net.microfalx.maven.model.SessionMetrics;
+import net.microfalx.metrics.Value;
 
+import java.time.temporal.Temporal;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.StringJoiner;
 
 import static java.util.Collections.unmodifiableCollection;
 import static java.util.Collections.unmodifiableList;
 import static net.microfalx.lang.ArgumentUtils.requireNonNull;
 import static net.microfalx.lang.FormatterUtils.formatNumber;
-import static net.microfalx.lang.StringUtils.COMMA_WITH_SPACE;
 import static net.microfalx.lang.StringUtils.EMPTY_STRING;
 
 public class ChartHelper {
@@ -110,25 +114,45 @@ public class ChartHelper {
         return chart;
     }
 
-    public static <V extends Number> String getSeries(Iterable<V> series) {
-        StringBuilder builder = new StringBuilder();
-        for (V value : series) {
-            StringUtils.append(builder, toString(value), COMMA_WITH_SPACE);
-        }
-        builder.insert(0, "[");
-        builder.append("]");
-        return builder.toString();
+    public AreaChart<Long, Float> getServerCpu(String id, Integer width) {
+        AreaChart<Long, Float> chart = new AreaChart<>(id, "Server / CPU");
+        chart.add(convert("System", session.getServer().get(ServerMetrics.CPU_SYSTEM)));
+        chart.add(convert("User", session.getServer().get(ServerMetrics.CPU_USER)));
+        chart.add(convert("Nice", session.getServer().get(ServerMetrics.CPU_NICE)));
+        chart.add(convert("I/O Wait", session.getServer().get(ServerMetrics.CPU_IO_WAIT)));
+        chart.setStacked(true);
+        return chart;
     }
 
-    public static String getLabels(Iterable<String> labels) {
-        StringBuilder builder = new StringBuilder();
-        for (String label : labels) {
-            label = "'" + label + "'";
-            StringUtils.append(builder, label, COMMA_WITH_SPACE);
+    public AreaChart<Long, Float> getServerMemory(String id, Integer width) {
+        AreaChart<Long, Float> chart = new AreaChart<>(id, "Server / Memory");
+        chart.add(convert("Maximum", session.getServer().get(ServerMetrics.MEMORY_MAX)));
+        chart.add(convert("Used", session.getServer().get(ServerMetrics.MEMORY_USED)));
+        return chart;
+    }
+
+    public AreaChart<Long, Float> getProcessCpu(String id, Integer width) {
+        AreaChart<Long, Float> chart = new AreaChart<>(id, "Process / CPU");
+        chart.add(convert("System", session.getJvm().get(VirtualMachineMetrics.CPU_SYSTEM)));
+        chart.add(convert("User", session.getJvm().get(VirtualMachineMetrics.CPU_USER)));
+        chart.setStacked(true);
+        return chart;
+    }
+
+    public AreaChart<Long, Float> getProcessMemory(String id, Integer width) {
+        AreaChart<Long, Float> chart = new AreaChart<>(id, "Process / Memory");
+        chart.add(convert("Heap", session.getJvm().get(VirtualMachineMetrics.MEMORY_HEAP_USED)));
+        chart.add(convert("Non-Heap", session.getJvm().get(VirtualMachineMetrics.MEMORY_NON_HEAP_USED)));
+        chart.setStacked(true);
+        return chart;
+    }
+
+    private static Series<Long, Float> convert(String name, net.microfalx.metrics.Series metricsSeries) {
+        Series<Long, Float> series = new Series<>(name);
+        for (Value value : metricsSeries.getValues()) {
+            series.add(value.getTimestamp(), round(value.asFloat()));
         }
-        builder.insert(0, "[");
-        builder.append("]");
-        return builder.toString();
+        return series;
     }
 
     private static <V extends Number> String toString(V value) {
@@ -139,37 +163,71 @@ public class ChartHelper {
         }
     }
 
-    public static class Json {
-
-        private final String value;
-
-        public Json(String value) {
-            this.value = value;
-        }
-
-        @Override
-        public String toString() {
+    private static float round(float value) {
+        if (value < 0.001) {
+            return 0;
+        } else {
             return value;
         }
     }
 
-    public static class Data<N extends Number> {
+    public static class Data<X, Y extends Number> {
 
-        private String x;
-        private N y;
+        private X x;
+        private Y y;
 
-        public Data(String x, N y) {
+        public Data(X x, Y y) {
             requireNonNull(x);
             this.x = x;
             this.y = y;
         }
 
-        public String getX() {
+        public X getX() {
             return x;
         }
 
-        public N getY() {
+        public Y getY() {
             return y;
+        }
+
+        @Override
+        public String toString() {
+            return new StringJoiner(", ", Data.class.getSimpleName() + "[", "]")
+                    .add("x=" + x)
+                    .add("y=" + y)
+                    .toString();
+        }
+    }
+
+    public static class Series<X, Y extends Number> implements Nameable {
+
+        private final String name;
+        private final Collection<Data<X, Y>> data = new ArrayList<>();
+
+        public Series(String name) {
+            requireNonNull(name);
+            this.name = name;
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+
+        public Collection<Data<X, Y>> getData() {
+            return data;
+        }
+
+        public Series<X, Y> add(Data<X, Y> data) {
+            this.data.add(data);
+            return this;
+        }
+
+        public Series<X, Y> add(X x, Y y) {
+            requireNonNull(x);
+            requireNonNull(y);
+            add(new Data<>(x, y));
+            return this;
         }
     }
 
@@ -226,23 +284,54 @@ public class ChartHelper {
         }
     }
 
-    public static abstract class DataChart<N extends Number> extends Chart {
+    public static abstract class DataChart<X, Y extends Number> extends Chart {
 
-        private Collection<Data<N>> data = new ArrayList<>();
+        private final Collection<Data<X, Y>> data = new ArrayList<>();
 
         public DataChart(String id, String name) {
             super(id, name);
         }
 
-        public Collection<Data<N>> getData() {
-            List<Data<N>> sorted = new ArrayList<>(data);
+        public Collection<Data<X, Y>> getData() {
+            List<Data<X, Y>> sorted = new ArrayList<>(data);
             sorted.sort((o1, o2) -> -Double.compare(o1.getY().doubleValue(), o2.getY().doubleValue()));
             return unmodifiableCollection(sorted);
         }
 
-        public DataChart<N> add(String x, N y) {
+        public DataChart<X, Y> add(X x, Y y) {
             requireNonNull(x);
             data.add(new Data<>(x, y));
+            return this;
+        }
+    }
+
+    public static abstract class MultiSeriesChart<X, Y extends Number> extends Chart {
+
+        private final List<Series<X, Y>> series = new ArrayList<>();
+
+        public MultiSeriesChart(String id, String name) {
+            super(id, name);
+        }
+
+        public List<Series<X, Y>> getSeries() {
+            return unmodifiableList(series);
+        }
+
+        public String getXDataType() {
+            for (Series<X, Y> s : series) {
+                for (Data<X, Y> data : s.getData()) {
+                    X x = data.getX();
+                    if (x instanceof Temporal || x instanceof Long) {
+                        return "datetime";
+                    }
+                }
+            }
+            return "datetime";
+        }
+
+        public MultiSeriesChart<X, Y> add(Series<X, Y> series) {
+            requireNonNull(series);
+            this.series.add(series);
             return this;
         }
     }
@@ -317,7 +406,25 @@ public class ChartHelper {
         }
     }
 
-    public static class TreeMapChart<N extends Number> extends DataChart<N> {
+    public static class AreaChart<X, Y extends Number> extends MultiSeriesChart<X, Y> {
+
+        private boolean stacked;
+
+        public AreaChart(String id, String name) {
+            super(id, name);
+        }
+
+        public boolean isStacked() {
+            return stacked;
+        }
+
+        public AreaChart<X, Y> setStacked(boolean stacked) {
+            this.stacked = stacked;
+            return this;
+        }
+    }
+
+    public static class TreeMapChart<N extends Number> extends DataChart<String, N> {
 
         public TreeMapChart(String id, String name) {
             super(id, name);
